59b6ad960df0d4cca545f755b7281afaa2182f42
arecaps <arecaplan@gmail.com>
2014-11-09 21:07:01 +0000

    send json

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 $(function () {
 
 	$('#submit').click(function () {
-			//$.post("getUser.php", {name: userName}, function (data) {
+		//var noteData = {};
+		//noteData[$('#id').val()] = "This is a note";
+		//console.log(noteData);
+		//var email = $('#id').val(),
+		var note = "This is a note";
+		var noteData= {email: $('#id').val() , note : note};
+		noteData = JSON.stringify(noteData);
+		console.log(noteData);
+			$.post("http://162.243.45.239/OneNote/recieveNotes.php ", noteData, function (data) {
 	        console.log("success");
-
-	    //});
+	        $('.form-signin').hide();
+	    });
 	});
 
 
