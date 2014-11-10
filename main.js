54df2e331611e374784a0228c24accfc4e6b4618
arecaps <arecaplan@gmail.com>
2014-11-10 05:41:05 +0000

    added ability to view notes

--- main.js
+++ main.js
@@ -1,23 +1,77 @@
 /*global $ */
 $(function () {
 
+	var email = '',
+			notes = [];
+
 	$('#submit').click(function () {
-		var note = "This is a note";
-		var noteData= {email: $('#id').val() , note : note};
-		console.log(noteData);
-			$.post("http://162.243.45.239/OneNote/recieveNotes.php ", noteData, function (data) {
-	        console.log("success");
-	        $('.form-signin').hide();
-	        $(".mainArea").load("welcomePartial.html");
-	    });
+		email = $('#id').val();
+    $('.form-signin').hide();
+    $(".mainArea").load("welcomePartial.html");
 	});
 
 	$("#new").click(function (){
-		$(".mainArea").load("newPartial.html");
-		setTimeout(function() {
-		 $("#noteName").focus();
-		}, 80);
-	})
+		if(email != ""){
+			$(".mainArea").load("newPartial.html", function() {
+				$("#noteName").focus();
+				saveNote();
+			});
+		} else {
+			alert("You must use a valid email address");
+		}
+	});
+
+	function saveNote() {
+		$('#save').click(function () {
+			var noteArea = $('#newNote');
+			var note = noteArea.val();
+			var noteData= {email: email , note : note};
+			console.log(noteData);
+				$.post("http://162.243.45.239/OneNote/recieveNotes.php ", noteData, function (data) {
+		        console.log("success");
+		        //noteArea.val() = "";
+		    });
+		});
+	};
+
+	$("#view").click(function (){
+		if(email != ""){
+			$(".mainArea").load("viewPartial.html", function() {
+				viewNote();
+			});
+		} else {
+			alert("You must use a valid email address");
+		}
+	});
+
+	function viewNote() {
+		var noteArea = $('#viewNote');
+		var noteData= {email: email};
+		var allNotes ='';
+		var noteArray = [];
+		console.log(noteData);
+			$.post("http://162.243.45.239/OneNote/retrieveNotes.php ", noteData, function (data) {
+	        notes = $.parseJSON(data);
+					var noteNumber = 1;
+	       	$.each(notes, function (key, value){
+	       		//allNotes = allNotes  + noteNumber++ + ". " + value.note + "\n \n";
+	       		var ellipses = "";
+	       		if(value.note.length > 20) {
+	       			var ellipses = "...";
+	       		}
+	       		var clippedNote = value.note.substring(0,20);
+	       		noteArray.push(value.note);
+	       		$("#index").append("<li>" + clippedNote + ellipses + "</li>")
+	       	});
+	       	$('#index li').click(function(){
+					  var noteIndex = $(this).index();
+	       		noteArea.val(noteArray[noteIndex]);
+					});
+	    });
+	    $('#edit').click(function () {
+	    	//Do something with the note
+		});
+	};
 
 
 		var file = '';
