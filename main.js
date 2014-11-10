9214ab9efdf6a4611696188abadd59b81c7167e1
arecaps <arecaplan@gmail.com>
2014-11-10 00:38:15 +0000

    fixed the js

--- main.js
+++ main.js
@@ -2,13 +2,8 @@
 $(function () {
 
 	$('#submit').click(function () {
-		//var noteData = {};
-		//noteData[$('#id').val()] = "This is a note";
-		//console.log(noteData);
-		//var email = $('#id').val(),
 		var note = "This is a note";
 		var noteData= {email: $('#id').val() , note : note};
-		noteData = JSON.stringify(noteData);
 		console.log(noteData);
 			$.post("http://162.243.45.239/OneNote/recieveNotes.php ", noteData, function (data) {
 	        console.log("success");
@@ -25,7 +20,7 @@ $(function () {
 	})
 
 
-		file = '';
+		var file = '';
     $.getJSON(file, function(data) {
     		console.log(data);
     });
